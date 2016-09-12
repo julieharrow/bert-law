95ca53101bf0eff18c08a673fbca3f3202c1f93e
Peter Baggish <p.baggish@gmail.com>
2016-09-11 20:16:11 -0400

    turbolinks again

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,7 +5,6 @@
     $('#textarea1').val('New Text');
     $('#textarea1').trigger('autoresize');
 
-    $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top }); 
 
   }); // end of document ready
 
