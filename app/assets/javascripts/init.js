56d4e6067d68adacd3121c53edefe3fc664838c8
Peter Baggish <p.baggish@gmail.com>
2016-09-11 20:08:59 -0400

    footer nav

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,9 +5,9 @@
     $('#textarea1').val('New Text');
     $('#textarea1').trigger('autoresize');
 
-
+    $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top }); 
 
   }); // end of document ready
-  
+
 
 })(jQuery); // end of jQuery name space
